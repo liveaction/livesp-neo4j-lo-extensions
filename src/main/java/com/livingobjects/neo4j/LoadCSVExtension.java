cacd14de9145e465a13c60407ad0c0db269f4a95
Frédéric Combes <frederic.combes@livingobjects.com>
2016-04-20 17:13:18 +0200

    feat(iwan): Improve body part read

--- src/main/java/com/livingobjects/neo4j/LoadCSVExtension.java
+++ src/main/java/com/livingobjects/neo4j/LoadCSVExtension.java
@@ -4,10 +4,8 @@ import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Slf4jReporter;
 import com.codahale.metrics.Timer;
 import com.livingobjects.neo4j.iwan.IWanTopologyLoader;
-import com.sun.jersey.multipart.BodyPart;
 import com.sun.jersey.multipart.MultiPart;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.PropertyNamingStrategy;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +17,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -32,8 +31,8 @@ public final class LoadCSVExtension {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoadCSVExtension.class);
 
-    private static final ObjectMapper JSON_MAPPER = new ObjectMapper()
-            .setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+    private static final MediaType TEXT_CSV_MEDIATYPE = MediaType.valueOf("text/csv");
+    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
 
     private final GraphDatabaseService graphDb;
 
@@ -52,14 +51,13 @@ public final class LoadCSVExtension {
     @Consumes("multipart/mixed")
     public Response loadCSV(MultiPart multiPart) throws IOException, ServletException {
         try (Timer.Context ignore = metrics.timer("loadCSV").time()) {
-            File csvEntity;
-            if (multiPart.getBodyParts().size() < 1) {
-                csvEntity = multiPart.getEntityAs(File.class);
-            } else {
-                BodyPart part = multiPart.getBodyParts().get(1);
-                csvEntity = part.getEntityAs(File.class);
-            }
-            try (InputStream is = new FileInputStream(csvEntity)) {
+            File csv = multiPart.getBodyParts().stream()
+                    .filter(bp -> TEXT_CSV_MEDIATYPE.equals(bp.getMediaType()))
+                    .map(bp -> bp.getEntityAs(File.class))
+                    .findAny()
+                    .orElseThrow(IllegalArgumentException::new);
+
+            try (InputStream is = new FileInputStream(csv)) {
                 Neo4jLoadResult result = new IWanTopologyLoader(graphDb, metrics).loadFromStream(is);
                 String json = JSON_MAPPER.writeValueAsString(result);
                 return Response.ok().entity(json).type(MediaType.APPLICATION_JSON).build();
@@ -72,8 +70,11 @@ public final class LoadCSVExtension {
                     return errorResponse(e);
                 }
             }
+
         } catch (IllegalArgumentException e) {
-            LOGGER.error("load-csv extension : bad input format", e);
+            String ex = JSON_MAPPER.writeValueAsString(new Neo4jErrorResult(e.getClass().getSimpleName(), e.getLocalizedMessage()));
+            return Response.status(Status.BAD_REQUEST).entity(ex).type(MediaType.APPLICATION_JSON_TYPE).build();
+        } catch (Exception e) {
             return errorResponse(e);
         } finally {
             reporter.report();
