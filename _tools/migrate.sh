4ceecc2a54910f0e3527af3e83151d294074fd5e
Frédéric Combes <frederic.combes@livingobjects.com>
2017-09-07 09:46:29 +0000

    feat(migrate): Improve the migration script

--- _tools/migrate.sh
+++ _tools/migrate.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+  then
+    echo "You most give the host and port for Neo4j Server (Ex: `migrate.sh localhost:7474`"
+fi
+
+host_port=$1
 temp_file=$(mktemp --tmpdir=/dev/shm/)
 
 trap "rm -rf ${temp_file}" EXIT
 
-curl -X POST "http://localhost:7474/unmanaged/migration/v2_00_0"
-echo ""
+status=$(curl -s -o ${temp_file} -w "%{http_code}" -X POST "http://${host_port}/unmanaged/migration/v2_00_0")
+if [ $status -eq 405 ]
+then
+    echo "Update Neo4j extension to the last version (>= 1.7)"
+    exit 1
+else
+    cat ${temp_file}
+    echo ""
+fi
 
 status=0;
 while [ ${status} -lt 200 ]; do
     sleep 2;
-    status=$(curl -s -o ${temp_file} -w "%{http_code}" "http://localhost:7474/unmanaged/migration/status")
+    status=$(curl -s -o ${temp_file} -w "%{http_code}" "http://${host_port}/unmanaged/migration/status")
     cat ${temp_file}
 done
 
