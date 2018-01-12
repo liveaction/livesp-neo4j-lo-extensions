5c198ca23dbdfa2ca236b7268b59d2e4e82d6b97
Augustin Gjini <augustin.gjini@gmail.com>
2018-01-12 16:05:17 +0000

    add local script to deploy to itg

--- deploy_to_local_neo4j.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[' | grep -v "Downloaded:" | grep -v "Downloading:"`
-
-mvn clean install && rm -f ~/applications/neo4j/plugins/* && tar xvfpz target/neo4j-lo-extensions-${VERSION}-plugin.tar.gz -C ~/applications/neo4j/plugins
-
-neo4j_stop && neo4j_start
