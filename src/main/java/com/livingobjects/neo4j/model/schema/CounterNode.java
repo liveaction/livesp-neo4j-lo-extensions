cf038af6959beabd348af22f0143cc28f73b6e37
Augustin Gjini <augustin.gjini@gmail.com>
2018-04-04 17:30:33 +0000

    feat(api): CounterNode now add optional description

--- src/main/java/com/livingobjects/neo4j/model/schema/CounterNode.java
+++ src/main/java/com/livingobjects/neo4j/model/schema/CounterNode.java
@@ -4,6 +4,9 @@ import com.google.common.base.MoreObjects;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @JsonIgnoreProperties(value = "label")
 public final class CounterNode {
 
@@ -12,42 +15,39 @@ public final class CounterNode {
     public final String defaultAggregation;
     public final String valueType;
     public final String name;
+    public final Optional<String> description;
 
     public CounterNode(@JsonProperty("unit") String unit,
                        @JsonProperty("defaultValue") String defaultValue,
                        @JsonProperty("defaultAggregation") String defaultAggregation,
                        @JsonProperty("valueType") String valueType,
-                       @JsonProperty("name") String name) {
+                       @JsonProperty("name") String name,
+                       @JsonProperty("description") Optional<String> description) {
         this.unit = unit;
         this.defaultValue = defaultValue;
         this.defaultAggregation = defaultAggregation;
         this.valueType = valueType;
         this.name = name;
+        this.description = description;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         CounterNode that = (CounterNode) o;
-
-        if (unit != null ? !unit.equals(that.unit) : that.unit != null) return false;
-        if (defaultValue != null ? !defaultValue.equals(that.defaultValue) : that.defaultValue != null) return false;
-        if (defaultAggregation != null ? !defaultAggregation.equals(that.defaultAggregation) : that.defaultAggregation != null)
-            return false;
-        if (valueType != null ? !valueType.equals(that.valueType) : that.valueType != null) return false;
-        return name != null ? name.equals(that.name) : that.name == null;
+        return Objects.equals(unit, that.unit) &&
+                Objects.equals(defaultValue, that.defaultValue) &&
+                Objects.equals(defaultAggregation, that.defaultAggregation) &&
+                Objects.equals(valueType, that.valueType) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description);
     }
 
     @Override
     public int hashCode() {
-        int result = unit != null ? unit.hashCode() : 0;
-        result = 31 * result + (defaultValue != null ? defaultValue.hashCode() : 0);
-        result = 31 * result + (defaultAggregation != null ? defaultAggregation.hashCode() : 0);
-        result = 31 * result + (valueType != null ? valueType.hashCode() : 0);
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
+
+        return Objects.hash(unit, defaultValue, defaultAggregation, valueType, name, description);
     }
 
     @Override
@@ -58,6 +58,7 @@ public final class CounterNode {
                 .add("defaultAggregation", defaultAggregation)
                 .add("valueType", valueType)
                 .add("name", name)
+                .add("description", description)
                 .toString();
     }
 }
