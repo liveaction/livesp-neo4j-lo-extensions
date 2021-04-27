5495125191ae3a864e3f66c2972aa1fd64e23085
Simon Daudin <sdaudin@liveaction.com>
2021-04-27 17:51:35 +0200

    Revert "fix(export): adapt filtering deserialization"
    This reverts commit d1aa087074bc7dbc126de34e9495f658d59a4a72.

--- src/main/java/com/livingobjects/neo4j/model/export/query/filter/ValueFilter.java
+++ src/main/java/com/livingobjects/neo4j/model/export/query/filter/ValueFilter.java
@@ -1,33 +1,11 @@
 package com.livingobjects.neo4j.model.export.query.filter;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.google.common.escape.Escaper;
-import com.google.common.escape.Escapers;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.annotation.Nullable;
 import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class ValueFilter {
-
-    private static final Pattern PATTERN = Pattern.compile("\\[(!?\\w+)](.*)");
-
-    public static final Escaper ESCAPER = Escapers.builder()
-            .addEscape('[', "\\[")
-            .addEscape('\\', "\\\\")
-            .addEscape('^', "\\^")
-            .addEscape('$', "\\$")
-            .addEscape('.', "\\.")
-            .addEscape('|', "\\|")
-            .addEscape('?', "\\?")
-            .addEscape('*', "\\*")
-            .addEscape('+', "\\+")
-            .addEscape('(', "\\(")
-            .addEscape(')', "\\)")
-            .addEscape('{', "\\{")
-            .addEscape('}', "\\}")
-            .build();
 
     public enum Operator {
         lt, lte, gt, gte, eq, contains, regex, is_null
@@ -37,9 +15,9 @@ public final class ValueFilter {
     public final Operator operator;
     public final Comparable value;
 
-    private ValueFilter(boolean not,
-                        Operator operator,
-                        Object value) {
+    private ValueFilter(@JsonProperty("not") boolean not,
+                        @JsonProperty("operator") Operator operator,
+                        @JsonProperty("value") Object value) {
         this.not = not;
         this.operator = operator;
         if (value != null) {
@@ -50,27 +28,6 @@ public final class ValueFilter {
             }
         } else {
             this.value = null;
-        }
-    }
-
-    @JsonCreator
-    public static ValueFilter fromString(String predicate) {
-        if (predicate == null) {
-            throw new IllegalArgumentException("Predicate must not be null");
-        }
-        Matcher matcher = PATTERN.matcher(predicate);
-        if (matcher.matches()) {
-            String arg = matcher.group(1);
-            boolean not = false;
-            if (arg.startsWith("!")) {
-                not = true;
-                arg = arg.substring(1);
-            }
-            Operator operator = Operator.valueOf(arg);
-            String value = matcher.group(2);
-            return new ValueFilter(not, operator, value);
-        } else {
-            return new ValueFilter(false, Operator.regex, "(?i).*" + ESCAPER.escape(predicate) + ".*");
         }
     }
 
